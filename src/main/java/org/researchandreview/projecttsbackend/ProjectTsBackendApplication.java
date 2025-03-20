849e1fbd35409315ca507c3076e36f80dc5b4ec0
hjs0410hc <hjs0410hc@gmail.com>
2025-03-21 03:51:54 +0900

    feat: initialized spring boot program

---
+++ src/main/java/org/researchandreview/projecttsbackend/ProjectTsBackendApplication.java
@@ -0,0 +1,13 @@
+package org.researchandreview.projecttsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectTsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/researchandreview/projecttsbackend/ProjectTsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.researchandreview.projecttsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
