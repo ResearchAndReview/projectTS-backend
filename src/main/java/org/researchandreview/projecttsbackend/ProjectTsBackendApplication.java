2d144a9dfbf2a4fdb93dd226d1146aa66678f2ce
hjs0410hc <s7s7111@naver.com>
2025-04-14 16:30:37 +0900

    feat: add swagger server

--- src/main/java/org/researchandreview/projecttsbackend/ProjectTsBackendApplication.java
+++ src/main/java/org/researchandreview/projecttsbackend/ProjectTsBackendApplication.java
@@ -1,8 +1,14 @@
 package org.researchandreview.projecttsbackend;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {
+        @Server(url = "https://js.thxx.xyz", description = "HTTPS (Secure)"),
+        @Server(url = "http://js.thxx.xyz", description = "HTTP (Insecure)")
+})
 @SpringBootApplication
 public class ProjectTsBackendApplication {
 
